1fc9e0fb1077fcaf3d5d8f431f138665f29964cf
241880573 <241880573@smail.nju.edu.cn>
2025-09-16 23:06:40 +0800

    25.9.16

---
+++ permutation_and_combination_consumption/components.h
@@ -0,0 +1,16 @@
+//
+// Created by li on 25-9-12.
+//
+
+#ifndef COMPONENTS_H
+#define COMPONENTS_H
+
+
+
+class components {
+
+};
+
+
+
+#endif //COMPONENTS_H
---
+++ permutation_and_combination_consumption/methords.h
@@ -0,0 +1,16 @@
+//
+// Created by li on 25-9-12.
+//
+
+#ifndef METHORDS_H
+#define METHORDS_H
+
+
+
+class methords {
+
+};
+
+
+
+#endif //METHORDS_H
